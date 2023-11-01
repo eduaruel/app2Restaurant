a895ae1415821c84fae66fcfc8769361a90ef0f9
Maykol <138355741+maykoldev@users.noreply.github.com>
2023-11-01 16:35:52 -0400

    actualizar3

--- views/register/registro.js
+++ views/register/registro.js
@@ -9,23 +9,54 @@ const btnRegistro = document.querySelector('#form-btn');
 // validacion con regex
 
 const nameVal = /^[A-Z]{1}[ a-zA-Z]+([a-zA-Z]+)?$/g;
+const emailVal = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+const passwordVal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!#$%&'*.+/=?^_`{|}~-]).{8,16}$/gm
 
 let valname = false;
+let valemail = false;
+let valpass = false;
+let valmatch = false;
 
-nameInput.addEventListener('input', e => {
+nameInput.addEventListener('input', e => {//debemos modificar los bordes 
     valname = nameVal.test(e.target.value);
-    console.log(valname);
-    if (valname) {
-        nameInput.classList.remove('focus:outline-blue-600', 'outline-4');
-        nameInput.classList.remove('focus:outline-red-700', 'outline-4');
-        nameInput.classList.add('focus:outline-green-700', 'outline-4');
-    } else if (nameInput.value === '') {
-        nameInput.classList.remove('focus:outline-greem-700', 'outline-4');
-        nameInput.classList.remove('focus:outline-red-700', 'outline-4');
-        nameInput.classList.add('focus:outline-blue-700', 'outline-4');
-    } else {
-        nameInput.classList.remove('focus:outline-blue-600', 'outline-4');
-        nameInput.classList.remove('focus:outline-blue-600', 'outline-4');
-        nameInput.classList.add('focus:outline-red-600', 'outline-4');
-    }
+    // console.log(valname);
+    validar(nameInput, valname)
+
 })
+
+emailInput.addEventListener('input', e => {
+    valemail = emailVal.test(e.target.value);
+    validar(emailInput, valemail);
+})
+passwordInput.addEventListener('input', e => {
+    valpass = passwordVal.test(e.target.value);
+    validar(passwordInput, valpass);
+    console.log(valpass);
+    validar(matchInput, valmatch)
+})
+
+matchInput.addEventListener('input', e => {
+    valmatch = e.target.value === passwordInput.value;
+    validar(matchInput, valmatch);
+    validad(passwordInput, valpass)
+})
+
+const validar = (input, value) => {
+    btnRegistro.disabled = valname && valpass && valmatch ? false : true;
+    console.log(btnRegistro.disabled);
+
+    if (value) {
+        input.classList.remove('focus:outline-blue-600', 'outline-4');
+        input.classList.remove('focus:outline-red-700', 'outline-4');
+        input.classList.add('focus:outline-green-700', 'outline-4');
+    } else if (input.value === '') {
+        input.classList.remove('focus:outline-green-700', 'outline-4');
+        input.classList.remove('focus:outline-red-700', 'outline-4');
+        input.classList.add('focus:outline-blue-600', 'outline-4');
+    } else {
+        input.classList.remove('focus:outline-blue-600', 'outline-4');
+        input.classList.remove('focus:outline-green-700', 'outline-4');
+        input.classList.add('focus:outline-red-700', 'outline-4');
+    }
+}
